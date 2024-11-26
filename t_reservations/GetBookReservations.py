022b144abc549929b8b6403cc00076239543cabc
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-11-26 13:05:33 -0500

    validacion de token para getBookReservations

--- t_reservations/GetBookReservations.py
+++ t_reservations/GetBookReservations.py
@@ -1,22 +1,46 @@
 import os
 import boto3
+import json
+import urllib.request
 from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
 
-    #body = event['body']
-
-    # por ahora se pasa el tenant y email por params, en realidad se conoce por el token
-
     query_params = event['query']
-
-    tenant_id = query_params['tenant_id']
-    email = query_params['email']
 
     if 'status' not in query_params:
         status = False
     else:
         status = query_params['status']
+
+    headers = event['headers']
+    token = headers['Authorization']
+
+    MU_url = f"https://n2tqx1stl1.execute-api.us-east-1.amazonaws.com/dev/tokens/validate"
+
+    data = {
+        "token": token
+    }
+
+    data_json = json.dumps(data).encode('utf-8')
+
+    request = urllib.request.Request(MU_url, data=data_json, method="POST")
+
+    request.add_header("Content-Type", "application/json")
+
+    with urllib.request.urlopen(request) as response:
+        user_response = json.loads(response.read())
+
+    if(user_response['statusCode'] == 403):
+        return {
+            "statusCode": 403,
+            "body": {
+                "message": "Token no válido"
+            }
+        }
+    
+    tenant_id = user_response['body']['tenant_id']
+    email = user_response['body']['email']
 
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ["RESERVATIONS_TABLE_NAME"])
